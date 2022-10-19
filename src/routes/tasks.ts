3fcb57d9fd800af4b1ff90c847e95b34f7c93840
AleEspindola <ale.espindola@outlook.com>
2022-10-19 20:15:10 -0300

    adding basic post routes

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -2,9 +2,22 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.route("/create")
-    .get((req: Request, res: Response)=>{
-        res.send("Recieved")
-    })
+import Task from "../models/Task";
+
+router
+  .route("/create")
+  .get((req: Request, res: Response) => {
+    res.render("tasks/create");
+  })
+  .post(async (req: Request, res: Response) => {
+    const { title, description } = req.body;
+    const newTask = new Task({ title, description });
+    await newTask.save();
+    res.send("Saved");
+  });
+
+router.route("/list").get((req: Request, res: Response) => {
+  res.render("tasks/list");
+});
 
 export default router;
